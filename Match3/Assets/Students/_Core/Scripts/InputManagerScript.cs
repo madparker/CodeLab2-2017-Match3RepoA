c45387c8aeba929304c85b3421d4c0fe2de89d58
GordonLee_Laptop <gordon.lee.6170@gmail.com>
2017-09-15 14:09:17 -0400

    Done "MatchManagerScript" and "InputManagerScript"

--- Match3/Assets/Students/_Core/Scripts/InputManagerScript.cs
+++ Match3/Assets/Students/_Core/Scripts/InputManagerScript.cs
@@ -12,23 +12,36 @@ public class InputManagerScript : MonoBehaviour {
 		gameManager = GetComponent<GameManagerScript>();
 	}
 
+    //Click on a token to to select;
 	public virtual void SelectToken(){
+        //On mouse left button clikced to select a token;
 		if(Input.GetMouseButtonDown(0)){
+            //Convert mouse position to world point (in position);
 			Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 			
+            //Get the collider of the mouse clicked point (postion);
 			Collider2D collider = Physics2D.OverlapPoint(mousePos);
 
+            //Check if there any token;
 			if(collider != null){
+
+                //If there is a token, get the token's game object;
 				if(selected == null){
+                    //If the first selected token is empty, "selected" stores the current tokan game object;
 					selected = collider.gameObject;
 				} else {
+                    //The first selected token position;
 					Vector2 pos1 = gameManager.GetPositionOfTokenInGrid(selected);
+                    //The current selected token positon;
 					Vector2 pos2 = gameManager.GetPositionOfTokenInGrid(collider.gameObject);
 
+                    //check the distance between the 2 selected tokens' position;
 					if(Mathf.Abs((pos1.x - pos2.x) + (pos1.y - pos2.y)) == 1){
+                        //If the distance is 1, do the position swap;
 						moveManager.SetupTokenExchange(selected, pos1, collider.gameObject, pos2, true);
 					}
 
+                    //Clear the "selected" to null;
 					selected = null;
 				}
 			}
@@ -36,6 +49,7 @@ public class InputManagerScript : MonoBehaviour {
 
 	}
 
+    //This is a ghost function that doing nothing! haha!
 	public int CommentFunc(int x, int y){
 		return x - y;
 	}
--- Match3/Assets/Students/_Core/Scripts/MatchManagerScript.cs
+++ Match3/Assets/Students/_Core/Scripts/MatchManagerScript.cs
@@ -9,13 +9,16 @@ public class MatchManagerScript : MonoBehaviour {
 		gameManager = GetComponent<GameManagerScript>();
 	}
 
+    //Check if Match and return a bool;
 	public virtual bool GridHasMatch(){
 		bool match = false;
 		
 		for(int x = 0; x < gameManager.gridWidth; x++){
 			for(int y = 0; y < gameManager.gridHeight ; y++){
 				if(x < gameManager.gridWidth - 2){
-					match = match || GridHasHorizontalMatch(x, y);
+
+                    //Check horizontally match by calling "GridHasHorizontalMatch()";
+                    match = match || GridHasHorizontalMatch(x, y);
 				}
 			}
 		}
@@ -23,66 +26,103 @@ public class MatchManagerScript : MonoBehaviour {
 		return match;
 	}
 
-	public bool GridHasHorizontalMatch(int x, int y){
+    //Check if there are 3 matching tokens in Horizontal grid; This method is called in "GridHasMatch()";
+    public bool GridHasHorizontalMatch(int x, int y){
 		GameObject token1 = gameManager.gridArray[x + 0, y];
 		GameObject token2 = gameManager.gridArray[x + 1, y];
 		GameObject token3 = gameManager.gridArray[x + 2, y];
-		
-		if(token1 != null && token2 != null && token3 != null){
-			SpriteRenderer sr1 = token1.GetComponent<SpriteRenderer>();
+
+        //Check the token sprite exist;
+        if (token1 != null && token2 != null && token3 != null) {
+            //Get "Sprite Renderer" from each token;
+            SpriteRenderer sr1 = token1.GetComponent<SpriteRenderer>();
 			SpriteRenderer sr2 = token2.GetComponent<SpriteRenderer>();
 			SpriteRenderer sr3 = token3.GetComponent<SpriteRenderer>();
-			
-			return (sr1.sprite == sr2.sprite && sr2.sprite == sr3.sprite);
+
+            //Check are these 3 sprite (token using this sprite) are matching;
+            return (sr1.sprite == sr2.sprite && sr2.sprite == sr3.sprite);
 		} else {
+            //if not, return false;
 			return false;
 		}
 	}
 
-	public int GetHorizontalMatchLength(int x, int y){
+    //This method is called in "RemoveMatches()";
+    //Return the lenth of matched tokens;
+    //(int x, int y) indecate the left most token position in the matched tokens;
+    public int GetHorizontalMatchLength(int x, int y){
+
+        //to count the length of match
 		int matchLength = 1;
 		
+        //store the left most token's game object;
 		GameObject first = gameManager.gridArray[x, y];
 
+        //Check the left most token is existed;
 		if(first != null){
 			SpriteRenderer sr1 = first.GetComponent<SpriteRenderer>();
 			
+            //Check the other tokens at right side are match and get the lenth of the matched tokens;
 			for(int i = x + 1; i < gameManager.gridWidth; i++){
-				GameObject other = gameManager.gridArray[i, y];
+                //get the game object from this grid
+                GameObject other = gameManager.gridArray[i, y];
 
+                //check if the game object exists;
 				if(other != null){
+
+                    //get the sprite renderer from the game object;
 					SpriteRenderer sr2 = other.GetComponent<SpriteRenderer>();
 
+                    //check if the sprite is the same as the left most token's sprite;
 					if(sr1.sprite == sr2.sprite){
+
+                        //increase the count for match;
 						matchLength++;
 					} else {
+                        //if the sprite is different, end the for loop;
 						break;
 					}
 				} else {
-					break;
+                    //if the game object dosen't exist, end the for loop;
+                    break;
 				}
 			}
 		}
 		
+        //Return the lenth of matched tokens as int;
 		return matchLength;
 	}
 
+    //Remove matched tokens;
 	public virtual int RemoveMatches(){
 		int numRemoved = 0;
 
+        //Go through all the grid to check the matched tokens horizontally;
 		for(int x = 0; x < gameManager.gridWidth; x++){
 			for(int y = 0; y < gameManager.gridHeight ; y++){
-				if(x < gameManager.gridWidth - 2){
 
-					int horizonMatchLength = GetHorizontalMatchLength(x, y);
+                //Discard 2 collums from the right most;
+                if (x < gameManager.gridWidth - 2){
 
+                    //Call "GetHorizontalMatchLength(int x, int y)";
+                    //Get the lenth of matched tokens;
+                    int horizonMatchLength = GetHorizontalMatchLength(x, y);
+
+                    //If at least 3 tokens are matching, get the number of tokens which will be removed;
 					if(horizonMatchLength > 2){
 
+                        //Destroy matched token game objects from the grid and get the number of removed tokens;
 						for(int i = x; i < x + horizonMatchLength; i++){
+                            //Get the game object of token that will be removed;
 							GameObject token = gameManager.gridArray[i, y]; 
+
+                            //Destory the game ojbect;
 							Destroy(token);
 
+                            //Reset the grid to null;
 							gameManager.gridArray[i, y] = null;
+
+                            //Increase the number of removed tokens;
 							numRemoved++;
 						}
 					}
@@ -90,6 +130,7 @@ public class MatchManagerScript : MonoBehaviour {
 			}
 		}
 		
+        //Return the number of tokens which will be removed;
 		return numRemoved;
 	}
 }
