e7b84a93e32161382c70ec12a956f77231056af2
Hang <whitewield@126.com>
2017-09-21 15:32:07 -0400

    DONE!

--- Match3/Assets/Students/hr1051/Scripts/HR_GameManagerScript.cs
+++ Match3/Assets/Students/hr1051/Scripts/HR_GameManagerScript.cs
@@ -5,6 +5,63 @@ using Hang;
 
 namespace Hang {
 	public class HR_GameManagerScript : GameManagerScript {
+		public virtual void Start () {
+			tokenTypes = (Object[])Resources.LoadAll("_Core/Tokens/");
+			gridArray = new GameObject[gridWidth, gridHeight];
+			matchManager = GetComponent<MatchManagerScript>();
+			MakeGrid();
+			inputManager = GetComponent<InputManagerScript>();
+			repopulateManager = GetComponent<RepopulateScript>();
+			moveTokenManager = GetComponent<MoveTokensScript>();
+		}
 
+		public override void Update () {
+			if (((HR_MoveTokensScript)(moveTokenManager)).GetIsMoving ())
+				return;
+			
+			if (!GridHasEmpty ()) {
+				if (matchManager.GridHasMatch ()) {
+					matchManager.RemoveMatches ();
+				} else {
+					inputManager.SelectToken ();
+				}
+			} else {
+				moveTokenManager.MoveTokensToFillEmptySpaces ();
+			}
+		}
+
+		void MakeGrid () {
+			grid = new GameObject ("TokenGrid");
+			//  Makes grid based on width and height variables
+			for (int x = 0; x < gridWidth; x++) {
+				for (int y = 0; y < gridHeight; y++) {
+					//  Fills in the grid with tokens
+					AddTokenToPosInGrid (x, y, grid);
+				}
+			}
+		}
+
+		public void AddTokenToPosInGrid(int x, int y, GameObject parent) {
+			Vector3 position = GetWorldPositionFromGridPosition (x, y);
+
+			int t_num = Random.Range (0, tokenTypes.Length);
+			//  Gets random token from the tokenType array
+			GameObject token = 
+				Instantiate (tokenTypes [t_num], 
+					position, 
+					Quaternion.identity) as GameObject;
+			token.transform.parent = parent.transform;
+			gridArray [x, y] = token;
+
+			if (x > 1 && matchManager.GridHasHorizontalMatch (x - 2, y)) {
+				t_num++;
+				token.GetComponent<SpriteRenderer> ().sprite = ((GameObject)tokenTypes [t_num]).GetComponent<SpriteRenderer> ().sprite;
+			}
+
+			if (y > 1 && ((HR_MatchManagerScript)matchManager).GridHasVerticalMatch (x, y - 2)) {
+				t_num++;
+				token.GetComponent<SpriteRenderer> ().sprite = ((GameObject)tokenTypes [t_num]).GetComponent<SpriteRenderer> ().sprite;
+			}
+		}
 	}
 }
