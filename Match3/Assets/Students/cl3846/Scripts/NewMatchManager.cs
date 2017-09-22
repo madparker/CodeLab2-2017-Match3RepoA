008e9014e5b4cbf8d96389ac0a426cce46e62595
GordonLee_Laptop <gordon.lee.6170@gmail.com>
2017-09-22 12:20:33 -0400

    optimized code

--- Match3/Assets/Students/cl3846/Scripts/NewMatchManager.cs
+++ Match3/Assets/Students/cl3846/Scripts/NewMatchManager.cs
@@ -4,26 +4,27 @@ using UnityEngine;
 
 public class NewMatchManager : MatchManagerScript {
 
-    private List<Vector2> destoryList = new List<Vector2>();
+    private List<Vector2> destroyPositions = new List<Vector2>();
 
     public override bool GridHasMatch() {
+        bool _isMatch = false;
         int _xGrid = gameManager.gridWidth - 2;
         int _yGrid = gameManager.gridHeight - 2;
 
-        for (int x = 0; x < gameManager.gridWidth; x++) {
-            for (int y = 0; y < gameManager.gridHeight; y++) {
-                if ((x < _xGrid && GridHasHorizontalMatch(x, y)) || (y < _yGrid && GridHasVerticalMatch(x, y))) {
-                    return true;
+        for (int i = 0; i < gameManager.gridWidth; i++) {
+            for (int j = 0; j < gameManager.gridHeight; j++) {
+                if ((i < _xGrid && GridHasHorizontalMatch(i, j)) || (j < _yGrid && GridHasVerticalMatch(i, j))) {
+                    _isMatch = true;
                 }
             }
         }
 
-        return false;
+        return _isMatch;
     }
-    public bool GridHasVerticalMatch(int x, int y) {
-        GameObject _token0 = gameManager.gridArray[x, y + 0];
-        GameObject _token1 = gameManager.gridArray[x, y + 1];
-        GameObject _token2 = gameManager.gridArray[x, y + 2];
+    public bool GridHasVerticalMatch(int _x, int _y) {
+        GameObject _token0 = gameManager.gridArray[_x, _y + 0];
+        GameObject _token1 = gameManager.gridArray[_x, _y + 1];
+        GameObject _token2 = gameManager.gridArray[_x, _y + 2];
 
         if (_token0 != null && _token1 != null && _token2 != null) {
             SpriteRenderer _sprite0 = _token0.GetComponent<SpriteRenderer>();
@@ -35,15 +36,15 @@ public class NewMatchManager : MatchManagerScript {
         }
     }
 
-    public int GetVerticalMatchLength(int x, int y) {
+    public int GetVerticalMatchLength(int _x, int _y) {
         int _matchLength = 1;
-        GameObject _first = gameManager.gridArray[x, y];
+        GameObject _first = gameManager.gridArray[_x, _y];
 
         if (_first != null) {
             SpriteRenderer _sprite0 = _first.GetComponent<SpriteRenderer>();
 
-            for (int i = y + 1; i < gameManager.gridHeight; i++) {
-                GameObject _other = gameManager.gridArray[x, i];
+            for (int i = _y + 1; i < gameManager.gridHeight; i++) {
+                GameObject _other = gameManager.gridArray[_x, i];
                 if (_other != null) {
                     SpriteRenderer _sprite1 = _other.GetComponent<SpriteRenderer>();
                     if (_sprite0.sprite == _sprite1.sprite) {
@@ -61,24 +62,24 @@ public class NewMatchManager : MatchManagerScript {
 
     public override int RemoveMatches() {
         int _numRemoved = 0;
-        int horizontalMatchCheck = gameManager.gridWidth - 2;
-        int verticalMatchCheck = gameManager.gridHeight - 2;
+        int _horizontalMatchCheck = gameManager.gridWidth - 2;
+        int _verticalMatchCheck = gameManager.gridHeight - 2;
         for (int x = 0; x < gameManager.gridWidth; x++) {
             for (int y = 0; y < gameManager.gridHeight; y++) {
 
-                if (x < horizontalMatchCheck) {
-                    int horizonMatchLength = GetHorizontalMatchLength(x, y);
-                    if (horizonMatchLength > 2) {
-                        for (int i = x; i < x + horizonMatchLength; i++) {
-                            destoryList.Add(new Vector2(i, y));
+                if (x < _horizontalMatchCheck) {
+                    int _horizonMatchLength = GetHorizontalMatchLength(x, y);
+                    if (_horizonMatchLength > 2) {
+                        for (int i = x; i < x + _horizonMatchLength; i++) {
+                            destroyPositions.Add(new Vector2(i, y));
                         }
                     }
                 }
-                if (y < verticalMatchCheck) {
-                    int verticalMatchLength = GetVerticalMatchLength(x, y);
-                    if (verticalMatchLength > 2) {
-                        for (int i = y; i < y + verticalMatchLength; i++) {
-                            destoryList.Add(new Vector2(x, i));
+                if (y < _verticalMatchCheck) {
+                    int _verticalMatchLength = GetVerticalMatchLength(x, y);
+                    if (_verticalMatchLength > 2) {
+                        for (int i = y; i < y + _verticalMatchLength; i++) {
+                            destroyPositions.Add(new Vector2(x, i));
                         }
                     }
                 }
@@ -86,7 +87,7 @@ public class NewMatchManager : MatchManagerScript {
         }
 
 
-        foreach (Vector2 t_removePos in destoryList) {
+        foreach (Vector2 t_removePos in destroyPositions) {
             GameObject _token = gameManager.gridArray[(int)t_removePos.x, (int)t_removePos.y];
 
             if (!_token) {
@@ -96,8 +97,7 @@ public class NewMatchManager : MatchManagerScript {
             gameManager.gridArray[(int)t_removePos.x, (int)t_removePos.y] = null;
             _numRemoved++;
         }
-        destoryList.Clear();
+        destroyPositions.Clear();
         return _numRemoved;
     }
 }
-
