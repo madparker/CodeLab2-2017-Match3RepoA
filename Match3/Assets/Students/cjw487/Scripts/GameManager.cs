93c23ae6effc763dbe3ac61aa0ceb806c54723ff
Christopher Williams <c.j.williams717@gmail.com>
2017-09-22 11:54:55 -0400

    Chris: Bumped up lerp speed to make jitter less obvious

--- Match3/Assets/Students/cjw487/Scripts/GameManager.cs
+++ Match3/Assets/Students/cjw487/Scripts/GameManager.cs
@@ -2,6 +2,51 @@
 {
     public class GameManager : GameManagerScript
     {
-       //   CODE TO BE WRITTEN
+        public override void Start()
+        {
+            base.Start();
+        }
+
+        public override void Update()
+        {
+            if (!GridHasEmpty())
+            {
+                if (matchManager.GridHasMatch())
+                {
+                    matchManager.RemoveMatches();
+                }
+                else
+                {
+                    inputManager.SelectToken();
+                }
+            }
+            else
+            {
+                if (!moveTokenManager.move)
+                {
+                    moveTokenManager.SetupTokenMove();
+                }
+                if (!moveTokenManager.MoveTokensToFillEmptySpaces())
+                {
+                    repopulateManager.AddNewTokensToRepopulateGrid();
+                }
+            }
+        }
+
+        public override bool GridHasEmpty()
+        {
+            for (int x = 0; x < gridWidth; x++)
+            {
+                for (int y = 0; y < gridHeight; y++)
+                {
+                    if (gridArray[x, y] == null)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
     }
 }
