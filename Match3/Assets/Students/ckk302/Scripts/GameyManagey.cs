58eff4007ad372b33c0cdb828504f1834e08c09a
kit <ckk302@nyu.edu>
2017-09-28 15:39:15 -0400

    score and highlight stuff

--- Match3/Assets/Students/ckk302/Scripts/GameyManagey.cs
+++ Match3/Assets/Students/ckk302/Scripts/GameyManagey.cs
@@ -6,7 +6,30 @@ namespace kitkat
 {
     public class GameyManagey : GameManagerScript
     {
+        public override void Start()
+        {
+            tokenTypes = (Object[])Resources.LoadAll("ckk302/");
+            gridArray = new GameObject[gridWidth, gridHeight];
+            MakeGrid();
+            matchManager = GetComponent<MatchManagerScript>();
+            inputManager = GetComponent<InputManagerScript>();
+            repopulateManager = GetComponent<RepopulateScript>();
+            moveTokenManager = GetComponent<MoveTokensScript>();
+        }
 
+        void MakeGrid()
+        {
+            grid = new GameObject("TokenGrid");
+            //  Makes grid based on width and height variables
+            for (int x = 0; x < gridWidth; x++)
+            {
+                for (int y = 0; y < gridHeight; y++)
+                {
+                    //  Fills in the grid with tokens
+                    AddTokenToPosInGrid(x, y, grid);
+                }
+            }
+        }
 
     }
 }
