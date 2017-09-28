1e31b0f3cc93340f6e4107d55acd90e053bdffab
Andrew Petersell <andrewpetersell@gmail.com>
2017-09-28 19:15:20 -0400

    Combo Multiplier

---
+++ Match3/Assets/Students/ajp678/Scripts/AndrewGameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AndrewGameManager : GameManagerScript {
+
+	public int multiplier;
+	
+	// Update is called once per frame
+	public override void Update () 
+	{
+		if(!GridHasEmpty())
+		{
+			if(matchManager.GridHasMatch())
+			{
+				matchManager.RemoveMatches();
+				GetComponent<AndrewMatchManager> ().comboMultiplier++;
+			}
+			else
+			{
+				inputManager.SelectToken();
+				GetComponent<AndrewMatchManager>().comboMultiplier = 0;
+			}
+		}
+		else
+		{
+			if(!moveTokenManager.move)
+			{
+				moveTokenManager.SetupTokenMove();
+			}
+			if(!moveTokenManager.MoveTokensToFillEmptySpaces())
+			{
+				repopulateManager.AddNewTokensToRepopulateGrid();
+			}
+		}
+	}
+}
